dfa23cbf2249bed3571e7d76cf08503dae6fadf8
Scott Blum <dragonsinth@gmail.com>
2018-03-27 19:00:33 -0400

    WIP: better?

--- google/appengine/tools/devappserver2/fsevents_file_watcher.py
+++ google/appengine/tools/devappserver2/fsevents_file_watcher.py
@@ -13,14 +13,18 @@
 #   $ pip install macfsevents
 #   $ cp mtime_file_watcher.py \
 #        sdk/google/appengine/tools/devappserver2/mtime_file_watcher.py
+import logging
 import os
-import time
-#from os.path import abspath, join
+import threading
+
+from google.appengine.tools.devappserver2 import watcher_common
 
 try:
+  import FSEvents
   from fsevents import Observer
   from fsevents import Stream
 except ImportError:
+  FSEvents = None
   Observer = None
   Stream = None
 
@@ -28,13 +32,66 @@ class FSEventsFileWatcher(object):
     SUPPORTS_MULTIPLE_DIRECTORIES = True
 
     def __init__(self, directories, **kwargs):
-        self._changes = _changes = []
+        self._directories = [os.path.abspath(d) for d in directories]
+        self._watcher_ignore_re = None
+        self._skip_files_re = None
+        self._changes = []
+        self._change_event = threading.Event()
         self.observer = Observer()
 
         def callback(event, mask=None):
-            _changes.append(event.name)
+            if not event.mask & (FSEvents.kFSEventStreamEventFlagItemCreated |
+                             FSEvents.kFSEventStreamEventFlagItemRemoved |
+                             FSEvents.kFSEventStreamEventFlagItemInodeMetaMod |
+                             FSEvents.kFSEventStreamEventFlagItemRenamed |
+                             FSEvents.kFSEventStreamEventFlagItemModified |
+                             FSEvents.kFSEventStreamEventFlagItemFinderInfoMod |
+                             FSEvents.kFSEventStreamEventFlagItemChangeOwner |
+                             FSEvents.kFSEventStreamEventFlagItemXattrMod):
+                return
 
-        self.stream = Stream(callback, file_events=True, *directories)
+            absolute_path = event.name
+            directory = next(d for d in self._directories if absolute_path.startswith(d))
+            skip_files_re = self._skip_files_re
+            watcher_ignore_re = self._watcher_ignore_re
+            if watcher_common.ignore_file(absolute_path, skip_files_re, watcher_ignore_re):
+                return
+
+            # We also want to ignore a path if we should ignore any directory
+            # that the path is in.
+            def _recursive_ignore_dir(dirname):
+                assert not os.path.isabs(dirname)  # or the while will never terminate
+                (dir_dirpath, dir_base) = os.path.split(dirname)
+                while dir_base:
+                    if watcher_common.ignore_dir(dir_dirpath, dir_base, skip_files_re):
+                        return True
+                    if watcher_common.ignore_dir(dir_dirpath, dir_base, watcher_ignore_re):
+                        return True
+                    (dir_dirpath, dir_base) = os.path.split(dir_dirpath)
+                return False
+
+            relpath = os.path.relpath(absolute_path, directory)
+            if _recursive_ignore_dir(os.path.dirname(relpath)):
+                return
+
+            logging.warning("Reloading instances due to change in %s", absolute_path)
+            self._changes.append(absolute_path)
+            self._change_event.set()
+
+        self.stream = Stream(callback, file_events=True, *self._directories)
+
+    def set_watcher_ignore_re(self, watcher_ignore_re):
+        """Allows the file watcher to ignore a custom pattern set by the user."""
+        self._watcher_ignore_re = watcher_ignore_re
+
+    def set_skip_files_re(self, skip_files_re):
+        """All re's in skip_files_re are taken to be relative to its base-dir."""
+        self._skip_files_re = skip_files_re
+
+
+    def _path_ignored(self, file_path):
+        """Determines if a path is ignored or not."""
+        return watcher_common.ignore_file(file_path, self._skip_files_re, self._watcher_ignore_re)
 
     @staticmethod
     def is_available():
@@ -44,11 +101,14 @@ class FSEventsFileWatcher(object):
         self.observer.schedule(self.stream)
         self.observer.start()
 
-    def changes(self, timeout=None):
-        time.sleep(0.1)
+    def changes(self, timeout=0):
+      try:
+        self._change_event.wait(timeout / 1000.0)
         changed = set(self._changes)
-        del self._changes[:]
         return changed
+      finally:
+        self._changes = []
+        self._change_event.clear()
 
     def quit(self):
         self.observer.unschedule(self.stream)
